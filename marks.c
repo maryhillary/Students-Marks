a3c3b7d8e226820bc863506c87aa72513771b261
maryhillary <hillaryjohn301@gmail.com>
2026-08-15 15:12:07 +0530

    added marks program

---
+++ marks.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int mark1, mark2, mark3;
+
+    printf("Enter 3 marks: ");
+    scanf("%d %d %d", &mark1, &mark2, &mark3);
+
+    printf("Total = %d\n", mark1 + mark2 + mark3);
+
+    return 0;
+}
