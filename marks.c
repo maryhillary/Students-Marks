c985d3b2867f0e2f48e9aca586f46441935909a8
maryhillary <hillaryjohn301@gmail.com>
2026-08-15 15:28:57 +0530

    added grade calculation

--- marks.c
+++ marks.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+char grade = 'A';
+
 
 int main() {
     int mark1, mark2, mark3;
 
     printf("Enter 3 marks: ");
     scanf("%d %d %d", &mark1, &mark2, &mark3);
-
+    printf("Grade = %c\n", grade);
     printf("Average = %.2f\n", (mark1 + mark2 + mark3) / 3.0);
 
     return 0;
