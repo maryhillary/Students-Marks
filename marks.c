02bd1c3c5ee17804d22fa604142102634f3021e9
maryhillary <hillaryjohn301@gmail.com>
2026-08-15 15:22:08 +0530

    added average calculation

--- marks.c
+++ marks.c
@@ -6,7 +6,7 @@ int main() {
     printf("Enter 3 marks: ");
     scanf("%d %d %d", &mark1, &mark2, &mark3);
 
-    printf("Total = %d\n", mark1 + mark2 + mark3);
+    printf("Average = %.2f\n", (mark1 + mark2 + mark3) / 3.0);
 
     return 0;
 }
